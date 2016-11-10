8f92a0433afb942b7c0b83c279f32db64dd158c9
Bridget Went <bwent@bowdoin.edu>
2016-11-10 16:42:12 -0500

    alsdkf

--- n-queens/NQueensMain.java
+++ n-queens/NQueensMain.java
@@ -1,10 +1,28 @@
 
+import java.lang.Integer;
+
 public class NQueensMain {
 
+
 	public static void main(String[] args) {
-		new NQueensClass();
-		
-		
+
+		int size = 0;
+		int maxSteps = 0;
+
+		if (args.length != 0) {
+	    try {
+
+	        size = Integer.parseInt(args[0]);
+					maxSteps = Integer.parseInt(args[1]);
+
+	    } catch (NumberFormatException e) {
+	        System.err.println("Argument" + args[0] + " must be an integer.");
+	        System.exit(1);
+	    }
+	}
+
+		new NQueensClass(size, maxSteps);
+
 	}
 
 }
